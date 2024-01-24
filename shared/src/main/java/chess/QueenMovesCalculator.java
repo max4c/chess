8eaf43c8ae82b706df158a5b90d607196630a8ef
Max Forsey <maxforsey@I-am-Wise.app.byu.edu>
2024-01-23 17:45:43 -0700

    passed rook, queen, and bishop tests

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,12 +1,25 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 public class QueenMovesCalculator extends HelperMethodClass{
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        //call the helpermethod 8 times
-        return null;
+        HashSet<ChessMove> moveSet = new HashSet<>();
+
+        //Bishop-like Options
+        moveSet.addAll(lineMoves(1,-1,myPosition,board));
+        moveSet.addAll(lineMoves(-1,-1,myPosition,board));
+        moveSet.addAll(lineMoves(-1,1,myPosition,board));
+        moveSet.addAll(lineMoves(1,1,myPosition,board));
+        //Rook-like Options
+        moveSet.addAll(lineMoves(0,1,myPosition,board));
+        moveSet.addAll(lineMoves(0,-1,myPosition,board));
+        moveSet.addAll(lineMoves(-1,0,myPosition,board));
+        moveSet.addAll(lineMoves(1,0,myPosition,board));
+
+        return moveSet;
     }
 }
