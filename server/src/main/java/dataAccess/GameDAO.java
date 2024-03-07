c02be9a3a35f4f72e8fe753a20f57bcd0f189ebf
max4c <hello@maxforsey.com>
2024-03-07 16:45:00 -0700

    join game, refixed tests, and started dataAccessTests

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -8,10 +8,10 @@ import java.util.Collection;
 public interface GameDAO {
     void deleteAllGames() throws DataAccessException;
     int createGame(String gameName)throws DataAccessException;
-    GameData getGame(int gameID);
+    GameData getGame(int gameID) throws DataAccessException;
 
-    void updateGame(int gameID, String fieldToUpdate, String newValue);
+    void updateGame(int gameID, String fieldToUpdate, String newValue) throws DataAccessException;
 
-    Collection<GameData> listGames();
+    Collection<GameData> listGames() throws DataAccessException;
 
 }
