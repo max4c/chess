584503fb1c71e594ca089423b73e9c6cc251eedf
max4c <signal@maxforsey.com>
2024-03-27 15:52:08 -0600

    added new files

--- client/src/main/java/ui/PreloginClient.java
+++ client/src/main/java/ui/PreloginClient.java
@@ -1,2 +1,49 @@
-package ui;public class Prelogin {
+package ui;
+
+import server.ServerFacade;
+
+import java.util.Arrays;
+import static ui.EscapeSequences.*;
+
+public class PreloginClient{
+    private final String serverUrl;
+    private final ServerFacade server;
+
+    public PreloginClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String register(String... params) throws Exception{
+        if(params.length >= 3){
+            return "time to create the server";
+        }
+        throw new Exception();
+    }
+
+    public String help() {
+        return "register <USERNAME> <PASSWORD> <EMAIL>\n" +
+                "login <USERNAME> <PASSWORD>\n" +
+                "quit" + SET_TEXT_COLOR_WHITE +
+                " - playing chess\n" + SET_TEXT_COLOR_BLUE +
+                "help" + SET_TEXT_COLOR_WHITE +
+                "- with possible commands\n";
+    }
+
+
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,2 +1,37 @@
-package ui;public class Repl {
+package ui;
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
+
+public class Repl {
+    private final PreloginClient preloginClient;
+
+    public Repl(String serverUrl) {
+        preloginClient = new PreloginClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC51 Welcome to the 240 chess. Type help to get started. \uD83D\uDC51");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = preloginClient.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + SET_TEXT_COLOR_WHITE + ">>> " + SET_TEXT_COLOR_GREEN );
+    }
+
 }
