af82f2c8a9c87b90e55b3ad5b4de64b5f6f6c06a
max4c <signal@maxforsey.com>
2024-04-16 20:11:55 -0600

    work on sending notifications

--- shared/src/main/java/webSocketMessages/userCommands/JoinObserver.java
+++ shared/src/main/java/webSocketMessages/userCommands/JoinObserver.java
@@ -5,6 +5,7 @@ public class JoinObserver extends UserGameCommand{
     private final int gameID;
     public JoinObserver(String authToken, int gameID) {
         super(authToken);
+        this.commandType = CommandType.JOIN_OBSERVER;
         this.gameID = gameID;
     }
 
--- shared/src/main/java/webSocketMessages/userCommands/Leave.java
+++ shared/src/main/java/webSocketMessages/userCommands/Leave.java
@@ -4,6 +4,7 @@ public class Leave extends UserGameCommand{
     private final int gameID;
     public Leave(String authToken, int gameID) {
         super(authToken);
+        this.commandType = CommandType.LEAVE;
         this.gameID = gameID;
     }
 
